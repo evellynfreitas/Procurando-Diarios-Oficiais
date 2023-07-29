afeb93aff03bece0e821814d0bcad2919d2a6287
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-07-29 00:07:15 -0300

    Add files via upload

--- PesquisandoDiarios/main.py
+++ PesquisandoDiarios/main.py
@@ -1,53 +1,81 @@
-from datetime import datetime
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
-from selenium.webdriver.support.ui import Select
+import customtkinter as ctk
+from pagina_diarios import PaginaDiarios
 
-data_inicial = "01/06/2023"
-data_final = "04/07/2023"
 
-#data_inicial = datetime.strptime(data_inicial, '%d/%m/%Y').date()
-#data_final = datetime.strptime(data_final, '%d/%m/%Y').date()
+def click():
+    pes = pesquisa.get()
+    data_ini = dataInicial.get()
+    data_fim = dataFinal.get()
 
-pesquisa = "tributos"
+    municipios_escolhidos = []
+    indice = 0
 
-pagina = 1
-lista_links = []
-lista_datas = []
+    for check in lista_checkbox:
+        if check.get() == 1:
+            municipios_escolhidos.insert(indice, check.cget('text'))
+
+    nova = PaginaDiarios(municipios_escolhidos, pes, data_ini, data_fim)
+
+    #print(lista.get())
+    #if lista.get() == 'Arraial do Cabo':
+
+    #    obj = Arraial(pesquisa.get(), dataInicial.get(), dataFinal.get())
+    #    diarios = obj.retornaDiarios()
+
+    #    nova_janela = ctk.CTk()
+     #   texto = ''
+     #   for n in range(len(diarios)):
+     #       texto += diarios[n][0] + " | " + diarios[n][1] + "\n"
+
+      #  label = ctk.CTkLabel(nova_janela, text=texto)
+      #  label.pack(padx=10, pady=10)
+      #  nova_janela.mainloop()
+
+
+janela = ctk.CTk()
+janela.geometry('600x500')
+janela.title('CAD-RECEITA')
+
+janela.resizable(False, False)
+
+label1 = ctk.CTkLabel(janela, text="Procurar nos Diários Oficiais")
+label1.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
+
+pesquisa = ctk.CTkEntry(janela, placeholder_text="Digite o que deseja pesquisar")
+pesquisa.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
+
+dataInicial = ctk.CTkEntry(janela, placeholder_text="Entre com a data inicial")
+dataInicial.grid(row=2, column=1, padx=10, pady=10, sticky="ew")
+
+dataFinal = ctk.CTkEntry(janela, placeholder_text="Entre com a data final")
+dataFinal.grid(row=3, column=1, padx=10, pady=10, sticky="ew")
+
+label2 = ctk.CTkLabel(janela, text="Escolha os municípios:")
+label2.grid(row=4, column=1, padx=10, pady=10, sticky="ew")
+
+municipios = ['Areal', 'Arraial do Cabo', 'Belford Roxo', 'Cabo Frio', 'Comendador Levy', 'Niteroi', 'Quatis',
+              'São Gonçalo', 'São João de Mereti', 'São José do Vale do Rio Preto', 'São Pedro da Aldeia', 'Sapucaia',
+              'Varre-Sai']
+
+coluna = 0
+linha = 5
 cont = 0
 
-link = "https://www.diariomunicipal.com.br/aemerj/pesquisar"
+lista_checkbox = []
 
-driver = webdriver.Chrome()
-driver.get(link)
-driver.implicitly_wait(5)
+for m in municipios:
+    checkbox = ctk.CTkCheckBox(janela, text=m, onvalue=1, offvalue=0)
+    checkbox.grid(row=linha, column=coluna, padx=5, pady=5, sticky="ew")
+    lista_checkbox.insert(cont, checkbox)
+    cont += 1
+    coluna += 1
+    if coluna == 3:
+        coluna = 0
+        linha += 1
 
-select = Select(driver.find_element(By.NAME, 'busca_avancada[entidadeUsuaria]'))
-select.select_by_value('425')
+botao = ctk.CTkButton(janela, text='Pesquisar', command=click)
 
-# texto da pesquisa
-driver.find_element(by=By.NAME, value='busca_avancada[texto]').clear()
-time.sleep(3)
-driver.find_element(by=By.NAME, value='busca_avancada[texto]').send_keys(pesquisa)
-time.sleep(3)
+botao.grid(row=linha+1, column=1, padx=10, pady=10, sticky="ew")
 
-# data inicial
-driver.find_element(by=By.NAME, value='busca_avancada[dataInicio]').clear()
-driver.find_element(by=By.NAME, value='busca_avancada[dataInicio]').send_keys(data_inicial)
-time.sleep(3)
-#data final
-driver.find_element(by=By.NAME, value='busca_avancada[dataFim]').clear()
-driver.find_element(by=By.NAME, value='busca_avancada[dataFim]').send_keys(data_final)
-time.sleep(3)
+janela.mainloop()
 
-botao = driver.find_element(by=By.NAME, value='busca_avancada[Enviar]')
-time.sleep(3)
-botao.click()
-driver.switch_to.window(driver.window_handles[0])
-driver.save_screenshot('./image.png')
-
-
-driver.quit()
