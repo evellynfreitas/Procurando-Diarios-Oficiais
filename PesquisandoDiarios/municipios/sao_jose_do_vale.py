a2abc780506c1476153cef04cab6a212e427691d
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-07-31 19:33:55 -0300

    Add files via upload

--- PesquisandoDiarios/municipios/sao_jose_do_vale.py
+++ PesquisandoDiarios/municipios/sao_jose_do_vale.py
@@ -2,39 +2,53 @@ import PdfReader
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-url = 'https://sjvriopreto.rj.gov.br/diario-oficial'
-driver = webdriver.Chrome()
-driver.get(url)
-pesquisa = 'gabriela'
 
-data_inicial = "01/07/2023"
-data_final = "20/07/2023"
+class SaoJose:
+    def __init__(self, pesquisa, data_inicial, data_final):
+        self.data_inicial = data_inicial
+        self.data_final = data_final
+        self.pesquisa = pesquisa
+        self.url = 'https://sjvriopreto.rj.gov.br/diario-oficial'
 
-cont = 0
-lista_pdfs = []
+    def retornaDiarios(self):
 
-input_data_inicial = driver.find_element(By.NAME, 'data_inicio')
-input_data_inicial.click()
-input_data_inicial.send_keys(data_inicial)
+        driver = webdriver.Chrome()
+        driver.get(self.url)
 
-input_data_final = driver.find_element(By.NAME, 'data_fim')
-input_data_final.click()
-input_data_final.send_keys(data_final)
+        diarios = []
+        cont = 0
 
-botao = driver.find_element(By.NAME, 'submit')
-botao.click()
-driver.implicitly_wait(3)
+        input_data_inicial = driver.find_element(By.NAME, 'data_inicio')
+        input_data_inicial.click()
+        input_data_inicial.send_keys(self.data_inicial)
 
-div = driver.find_element(By.XPATH, '//*[@id="conteudo"]/div/div')
-lista = div.find_elements(By.TAG_NAME, 'a')
+        input_data_final = driver.find_element(By.NAME, 'data_fim')
+        input_data_final.click()
+        input_data_final.send_keys(self.data_final)
 
-for li in lista:
-    if li.get_attribute('class') == 'btn btn-primary btn-lg':
-        if PdfReader.contemPalavra(li.get_attribute('href'), pesquisa):
-            lista_pdfs.insert(cont, li.get_attribute('href'))
-            cont += 1
+        botao = driver.find_element(By.NAME, 'submit')
+        botao.click()
+        driver.implicitly_wait(3)
 
-driver.quit()
+        div = driver.find_element(By.XPATH, '//*[@id="conteudo"]/div/div')
+        lista = div.find_elements(By.TAG_NAME, 'a')
 
-for p in lista_pdfs:
-    print(p)
+        for a in lista:
+            if a.get_attribute('class') == 'btn btn-primary btn-lg':
+                link = a.get_attribute('href')
+                if PdfReader.contemPalavra(link, self.pesquisa):
+                    data = ''
+                    for c in a.get_attribute('title'):
+                        if c == '_':
+                            data += '/'
+                        elif c == '.':
+                            data += '/2023'
+                            break
+                        else:
+                            data += c
+
+                    diarios.insert(cont, [data, link])
+                    cont += 1
+
+        driver.quit()
+        return diarios
