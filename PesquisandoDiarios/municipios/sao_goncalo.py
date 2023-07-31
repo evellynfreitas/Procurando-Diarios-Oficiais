86b25d2cbbac4ddaa3d9a99deb4ae163ad16af63
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-07-31 19:33:34 -0300

    Add files via upload

--- PesquisandoDiarios/municipios/sao_goncalo.py
+++ PesquisandoDiarios/municipios/sao_goncalo.py
@@ -1,37 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
 
-continuar = True
-data_inicial = "2023-05-01"
-data_final = "2023-06-30"
-pesquisa = "poder+executivo"
-# palavras são pesquisadas separadas por +
-pagina = 1
-lista_links = []
-lista_datas = []
-cont = 0
 
-while continuar:
+class SaoGoncalo:
+    def __init__(self, pesquisa, data_inicial, data_final):
 
-    link = "https://www.saogoncalo.rj.gov.br/diario-oficial/?dataInicial=" + data_inicial + "&dataFinal=" + data_final + "&termo=" + pesquisa + "&btnPesquisarPorTermo=1&pagina="+str(pagina)
-    site = requests.get(link)
-    site = BeautifulSoup(site.text, "html.parser")
-    divs = site.find_all('div', class_='alert alert-secondary')
+        data_inicial = data_inicial.split('/')
+        self.data_inicial = data_inicial[2] + "-" + data_inicial[1] + '-' + data_inicial[0]
 
-    if not divs:
-        continuar = False
-    else:
-        for div in divs:
-            a = div.find('a')
-            lista_links.insert(cont, a['href'])
-            lista_datas.insert(cont, a.text)
-            # print(lista_datas[cont] + " | " + lista_links[cont])
-            cont = cont + 1
+        data_final = data_final.split('/')
+        self.data_final = data_final[2] + "-" + data_final[1] + '-' + data_final[0]
 
-    pagina = pagina + 1
+        self.pesquisa = ''
+        pesquisa = pesquisa.split(' ')
+        for palavra in pesquisa:
+            self.pesquisa += (palavra + "+")
 
+    def retornaDiarios(self):
 
-i = 0
-while i < cont:
-    print(lista_datas[i] + " | " + lista_links[i])
-    i = i + 1
+        diarios = []
+        continuar = True
+        pagina = 1
+        cont = 0
+
+        while continuar:
+
+            link = "https://www.saogoncalo.rj.gov.br/diario-oficial/?dataInicial=" + self.data_inicial + "&dataFinal="\
+                   + self.data_final + "&termo=" + self.pesquisa + "&btnPesquisarPorTermo=1&pagina="+str(pagina)
+            site = requests.get(link)
+            site = BeautifulSoup(site.text, "html.parser")
+            divs = site.find_all('div', class_='alert alert-secondary')
+
+            if not divs:
+                continuar = False
+            else:
+                for div in divs:
+                    a = div.find('a')
+                    data = a.text
+                    link = a['href']
+                    diarios.insert(cont, [data, link])
+                    cont += 1
+            pagina += 1
+        return diarios
