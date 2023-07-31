016a5597324e0c82710752ff7db83979d05f478d
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-07-31 19:35:01 -0300

    Add files via upload

--- PesquisandoDiarios/municipios/varre_sai.py
+++ PesquisandoDiarios/municipios/varre_sai.py
@@ -1,43 +1,54 @@
 import PdfReader
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
-url = 'https://varresai.rj.gov.br/site/diarios_oficiais'
-driver = webdriver.Chrome()
-driver.get(url)
-pesquisa = 'EXECUTIVO'
 
-data_inicial = "01/07/2023"
-data_final = "10/07/2023"
+class VarreSai:
+    def __init__(self, pesquisa, data_inicial, data_final):
+        self.pesquisa = pesquisa
+        self.data_inicial = data_inicial
+        self.data_final = data_final
+        self.url = 'https://varresai.rj.gov.br/site/diarios_oficiais'
 
-cont = 0
-lista_pdfs = []
-driver.implicitly_wait(5)
+    def retornaDiarios(self):
 
-input_data_inicial = driver.find_element(By.XPATH, '//*[@id="data_pub"]')
-input_data_inicial.click()
-input_data_inicial.send_keys(data_inicial)
+        diarios = []
+        cont = 0
 
-input_data_final = driver.find_element(By.XPATH, '//*[@id="data_pub_2"]')
-input_data_final.click()
-input_data_final.send_keys(data_final)
+        driver = webdriver.Chrome()
+        driver.get(self.url)
+        time.sleep(2)
 
-botao = driver.find_element(By.XPATH, '//*[@id="bt_lic_filtro"]')
-botao.click()
+        input_data_inicial = driver.find_element(By.XPATH, '//*[@id="data_pub"]')
+        input_data_inicial.click()
+        input_data_inicial.send_keys(self.data_inicial)
 
-div = driver.find_element(By.XPATH, '//*[@id="content"]/div[4]/div/div[1]/div')
-lista = div.find_elements(By.TAG_NAME, 'a')
+        input_data_final = driver.find_element(By.XPATH, '//*[@id="data_pub_2"]')
+        input_data_final.click()
+        input_data_final.send_keys(self.data_final)
 
-for a in lista:
-    try:
-        if 'btn btn-default' == str(a.get_attribute('class')):
-            if PdfReader.contemPalavra(a.get_attribute('href'), pesquisa):
-                lista_pdfs.insert(cont, a.get_attribute('href'))
-                cont += 1
-    except:
-        continue
+        botao = driver.find_element(By.XPATH, '//*[@id="bt_lic_filtro"]')
+        botao.click()
 
-driver.quit()
+        div = driver.find_element(By.XPATH, '//*[@id="content"]/div[4]/div/div[1]/div')
+        lista = div.find_elements(By.TAG_NAME, 'a')
+        h3 = div.find_elements(By.TAG_NAME, 'h3')
 
-for link in lista_pdfs:
-    print(link)
+        indiceh3 = len(h3)
+
+        for a in lista:
+
+            try:
+                if 'btn btn-default' == str(a.get_attribute('class')):
+                    indiceh3 -= 1
+                    link = a.get_attribute('href')
+                    if PdfReader.contemPalavra(link, self.pesquisa):
+                        data = h3[indiceh3].get_attribute('innerText').split()[4]
+                        diarios.insert(cont, [data, link])
+                        cont += 1
+            except:
+                continue
+
+        driver.quit()
+        return diarios
