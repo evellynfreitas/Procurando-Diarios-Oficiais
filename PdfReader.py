617912ae59e08843b96d0a9ced8a001de420902e
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-07-31 19:36:18 -0300

    Add files via upload

---
+++ PdfReader.py
@@ -0,0 +1,20 @@
+import io
+import requests
+import PyPDF2
+
+
+def contemPalavra(link, palavra):
+    try:
+        r = requests.get(link)
+        f = io.BytesIO(r.content)
+        reader = PyPDF2.PdfReader(f)
+        paginas = reader.pages
+        for pagina in paginas:
+            texto = pagina.extract_text()
+            if palavra.lower() in texto.lower():
+                return True
+
+        return False
+    except PyPDF2.errors.PdfReadError:
+        return False
+
