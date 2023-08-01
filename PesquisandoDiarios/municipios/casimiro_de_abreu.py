eafb7a316ae7de7c4779a4689a0fc1703bf23b51
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-08-01 20:52:15 -0300

    Novo município Casimiro de Abreu

---
+++ PesquisandoDiarios/municipios/casimiro_de_abreu.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+import PdfReader
+
+
+class Casimiro:
+    def __init__(self, pesquisa, data_inicial, data_final):
+        self.data_inicial = data_inicial
+        self.data_final = data_final
+        self.pesquisa = pesquisa
+        self.url = 'https://transparencia.casimirodeabreu.rj.gov.br/jornal.php?Num=&Sec=&dtini=' + data_inicial + \
+                   '&dtfim=' + data_final + '&pagina='
+
+    def retornaDiarios(self):
+        diarios = []
+        cont = 0
+        paginas = 0
+
+        while True:
+            site = requests.get(self.url+str(paginas))
+            site = BeautifulSoup(site.text, "html.parser")
+            lista_a = site.find_all('a', class_='btn btn-primary btn-sm')
+            if len(lista_a) == 0:
+                break
+            for a in lista_a:
+                url = 'https://transparencia.casimirodeabreu.rj.gov.br/' + a['href']
+                site = requests.get(url)
+                site = BeautifulSoup(site.text, "html.parser")
+
+                codigo = site.find('a', class_='btn btn-primary btn-sm pull-right')['href']
+                link = 'https://transparencia.casimirodeabreu.rj.gov.br/' + codigo.replace(' ', '%20')
+                data = str(site.find('strong')).split(' ')[3]
+
+                if PdfReader.contemPalavra(link, self.pesquisa):
+                    diarios.insert(cont, [data, link])
+                    cont += 1
+
+            paginas += 1
+        return diarios
