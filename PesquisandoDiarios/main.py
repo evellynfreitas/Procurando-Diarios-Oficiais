243f64641a5f042541189f0ee409074c2203c444
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-08-02 22:19:36 -0300

    Inserindo 'Iguaba'

--- PesquisandoDiarios/main.py
+++ PesquisandoDiarios/main.py
@@ -18,29 +18,30 @@ def click():
 
 
 janela = ctk.CTk()
-janela.geometry('600x500')
+janela.geometry('550x500')
 janela.title('CAD-RECEITA')
-janela.iconbitmap('recursos/lupa.ico')
+
+# janela.iconbitmap('recursos/lupa.ico')
 janela.resizable(False, False)
 
 label1 = ctk.CTkLabel(janela, text="Procurar nos Diários Oficiais")
 label1.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
 
-pesquisa = ctk.CTkEntry(janela, placeholder_text="Digite o que deseja pesquisar")
+pesquisa = ctk.CTkEntry(janela, placeholder_text="Digite a palavra chave")
 pesquisa.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
 
-dataInicial = ctk.CTkEntry(janela, placeholder_text="Entre com a data inicial")
+dataInicial = ctk.CTkEntry(janela, placeholder_text="Digite a data inicial")
 dataInicial.grid(row=2, column=1, padx=10, pady=10, sticky="ew")
 
-dataFinal = ctk.CTkEntry(janela, placeholder_text="Entre com a data final")
+dataFinal = ctk.CTkEntry(janela, placeholder_text="Digite a data final")
 dataFinal.grid(row=3, column=1, padx=10, pady=10, sticky="ew")
 
 label2 = ctk.CTkLabel(janela, text="Escolha os municípios:")
 label2.grid(row=4, column=1, padx=10, pady=10, sticky="ew")
 
-municipios = ['Areal', 'Arraial do Cabo', 'Belford Roxo', 'Cabo Frio', 'Comendador Levy', 'Niteroi', 'Quatis',
-              'São Gonçalo', 'São João de Mereti', 'São José do Vale do Rio Preto', 'São Pedro da Aldeia', 'Sapucaia',
-              'Varre-Sai']
+municipios = ['Areal', 'Arraial do Cabo', 'Belford Roxo', 'Búzios', 'Cabo Frio', 'Casimiro de Abreu',
+              'Comendador Levy', 'Cordeiro', 'Iguaba Grande', 'Niteroi', 'Quatis', 'São Gonçalo', 'São João de Mereti',
+              'São José do Vale do Rio Preto', 'São Pedro da Aldeia', 'Sapucaia', 'Varre-Sai']
 
 coluna = 0
 linha = 5
@@ -63,3 +64,4 @@ botao = ctk.CTkButton(janela, text='Pesquisar', command=click)
 botao.grid(row=linha+1, column=1, padx=10, pady=10, sticky="ew")
 
 janela.mainloop()
+# pyinstaller --onefile --icon=recursos/lupa.ico main.py
