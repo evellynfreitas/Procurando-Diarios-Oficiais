ff9feb223ba8f2845649b7b57d471e77c3542efb
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-08-11 22:44:31 -0300

    Atualização em Arraial

--- PesquisandoDiarios/municipios/arraial_do_cabo.py
+++ PesquisandoDiarios/municipios/arraial_do_cabo.py
@@ -4,40 +4,32 @@ import requests
 
 class Arraial:
     def __init__(self, pesquisa, data_inicial, data_final):
-        self.pesquisa = pesquisa
+        if pesquisa == '' or pesquisa == ' ':
+            self.pesquisa = '0'
+        else:
+            self.pesquisa = pesquisa
 
-        data_nova = ''
-        for i in list(data_inicial):
-            if i == '/':
-                i = '-'
-            data_nova += i
-        self.data_inicial = data_nova
+        self.data_inicial = data_inicial.replace('/', '-')
+        self.data_final = data_final.replace('/', '-')
 
-        data_nova = ''
-        for i in list(data_final):
-            if i == '/':
-                i = '-'
-            data_nova += i
-        self.data_final = data_nova
+        self.url = "https://www.arraial.rj.gov.br/portal/diario-oficial/1"
+        self.url = f"{self.url}/{self.data_inicial}/{self.data_final}/{self.pesquisa}/0/0/"
 
     def retornaDiarios(self):
-        link = "https://www.arraial.rj.gov.br/portal/diario-oficial/1/"
-        link += self.data_inicial + "/" + self.data_final + "/" + self.pesquisa + "/0/0/"
-        site = requests.get(link)
+        site = requests.get(self.url)
         site = BeautifulSoup(site.text, "html.parser")
 
         diarios = []
-        cont = 0
 
         divs = site.find_all('div', class_='dof_publicacao_diario sw_item_listagem')
+
         for div in divs:
             lista_a = div.find_all('a', href=True)
             span = div.find('span', class_='sw_descricao_info')
 
             for a in lista_a:
                 data = span.find('span').text.split(' ')[0]
-                href = "https://www.arraial.rj.gov.br" + a['href']
+                link = "https://www.arraial.rj.gov.br" + a['href']
 
-                diarios.insert(cont, [data, href])
-                cont += 1
+                diarios.append([data, link])
         return diarios
