e600f7338de675fd66a90f51bd024f8821aabadb
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-08-11 22:48:25 -0300

    Atualizando Búzios

--- PesquisandoDiarios/municipios/buzios.py
+++ PesquisandoDiarios/municipios/buzios.py
@@ -1,4 +1,3 @@
-buzios.py
 from bs4 import BeautifulSoup
 import requests
 import PdfReader
@@ -9,32 +8,36 @@ class Buzios:
         self.data_inicial = data_inicial
         self.data_final = data_final
         self.pesquisa = pesquisa
-        self.url = 'https://buzios.aexecutivo.com.br/jornal.php?Num=&Sec=&dtini=' + data_inicial + \
-                   '&dtfim=' + data_final + '&pagina='
+        self.url = 'https://buzios.aexecutivo.com.br/jornal.php?Num=&Sec='
+        self.url = f"{self.url}&dtini={self.data_inicial}&dtfim={self.data_final}&pagina="
 
     def retornaDiarios(self):
+
         diarios = []
-        cont = 0
         paginas = 0
 
         while True:
             site = requests.get(self.url+str(paginas))
             site = BeautifulSoup(site.text, "html.parser")
+
             lista_a = site.find_all('a', class_='btn btn-primary btn-sm')
-            if len(lista_a) == 0:
+            if len(lista_a) == 0:  # nao achou nenhum diário
                 break
+
             for a in lista_a:
-                url = 'https://buzios.aexecutivo.com.br/' + a['href']
+                url = f"https://buzios.aexecutivo.com.br/{a['href']}"
                 site = requests.get(url)
                 site = BeautifulSoup(site.text, "html.parser")
 
                 codigo = site.find('a', class_='btn btn-primary btn-sm pull-right')['href']
-                link = 'https://buzios.aexecutivo.com.br/' + codigo.replace(' ', '%20')
+                codigo = codigo.replace(' ', '%20')
+                link = f"https://buzios.aexecutivo.com.br/{codigo}"
+
                 data = str(site.find('strong')).split(' ')[3]
 
                 if PdfReader.contemPalavra(link, self.pesquisa):
-                    diarios.insert(cont, [data, link])
-                    cont += 1
+                    diarios.append([data, link])
 
             paginas += 1
+
         return diarios
