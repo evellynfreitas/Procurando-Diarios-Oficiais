b7bb330627ba6793d5fe32f2e070baa7262ac647
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-08-11 22:49:02 -0300

    Update casimiro_de_abreu.py

--- PesquisandoDiarios/municipios/casimiro_de_abreu.py
+++ PesquisandoDiarios/municipios/casimiro_de_abreu.py
@@ -8,32 +8,35 @@ class Casimiro:
         self.data_inicial = data_inicial
         self.data_final = data_final
         self.pesquisa = pesquisa
-        self.url = 'https://transparencia.casimirodeabreu.rj.gov.br/jornal.php?Num=&Sec=&dtini=' + data_inicial + \
-                   '&dtfim=' + data_final + '&pagina='
+        self.url = 'https://transparencia.casimirodeabreu.rj.gov.br/jornal.php?Num=&Sec='
+        self.url = f"{self.url}&dtini={self.data_inicial}&dtfim={self.data_final}&pagina="
 
     def retornaDiarios(self):
+
         diarios = []
-        cont = 0
         paginas = 0
 
         while True:
             site = requests.get(self.url+str(paginas))
             site = BeautifulSoup(site.text, "html.parser")
+
             lista_a = site.find_all('a', class_='btn btn-primary btn-sm')
             if len(lista_a) == 0:
                 break
+
             for a in lista_a:
                 url = 'https://transparencia.casimirodeabreu.rj.gov.br/' + a['href']
                 site = requests.get(url)
                 site = BeautifulSoup(site.text, "html.parser")
 
                 codigo = site.find('a', class_='btn btn-primary btn-sm pull-right')['href']
-                link = 'https://transparencia.casimirodeabreu.rj.gov.br/' + codigo.replace(' ', '%20')
+                codigo = codigo.replace(' ', '%20')
+
+                link = 'https://transparencia.casimirodeabreu.rj.gov.br/' + codigo
                 data = str(site.find('strong')).split(' ')[3]
 
                 if PdfReader.contemPalavra(link, self.pesquisa):
-                    diarios.insert(cont, [data, link])
-                    cont += 1
-
+                    diarios.append([data, link])
             paginas += 1
+
         return diarios
